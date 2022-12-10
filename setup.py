eb754fc99ff9af61cea950c0813212cf2e818199
Akihiro Kuroiwa <akuroiwa@env-reform.com>
2022-12-10 15:23:49 +0900

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chem_ant',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/akuroiwa/chem-ant',
     # # PyPI url
     # download_url='',
