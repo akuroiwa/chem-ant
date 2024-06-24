3f79cedcf7bb2eb161e7cf9da1b8d0b36669a65a
Akihiro Kuroiwa <akuroiwa@env-reform.com>
2024-06-24 17:51:13 +0900

    The new package rdkit supports Python versions 3.8 through 3.12.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=open("README.md", "r").read(),
     long_description_content_type='text/markdown',
     zip_safe=False,
-    python_requires=">=3.7, !=3.10.*",
+    python_requires=">=3.8, !=3.10.*",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
