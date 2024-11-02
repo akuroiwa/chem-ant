b0cc2905d281212f0b1fc0e6503063fe02a585a7
Akihiro Kuroiwa <akuroiwa@env-reform.com>
2024-11-02 20:20:28 +0900

    The AutoDock Vina log option has been deprecated.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chem_ant',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/akuroiwa/chem-ant',
     # # PyPI url
     # download_url='',
